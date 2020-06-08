5ab2e9701b9766cd4cd09bfeda9bdd6eb963f603
Nenad Misic <nenad.misic997@gmail.com>
2020-06-09 00:58:19 +0200

    Spiring integrated with drools project

---
+++ boot-drools/src/main/java/com/sbnz/model/CationGroup.java
@@ -0,0 +1,20 @@
+package com.sbnz.model;
+
+public class CationGroup {
+    String code;
+
+    public CationGroup() {
+    }
+
+    public CationGroup(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
